f8c8bf32ad17ceefceeff3537560684732b6022d
ratanratan <ratna5256@gmail.com>
2026-08-18 19:55:53 +0530

    home page uupdate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <>
       <h1>This is Home Page...</h1>
+      <h1>This is Home Page After github Modification...</h1>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
